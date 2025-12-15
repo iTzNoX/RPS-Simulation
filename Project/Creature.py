7f3fe74df1fc0d0f0295f943b44dfd325577a9ca
Timothy Kropp <timothy.kropp@fcbup.de>
2025-12-15 17:49:09 +0100

    adds Creature Class

---
+++ Project/Creature.py
@@ -0,0 +1,18 @@
+
+# Handles the creation and actions of the Creatures
+class Creature:
+    id_counter = 0
+
+    def __init__(self, strategy: str):
+        Creature.increase_id_counter()
+        self.id = f"{Creature.id_counter:06d}"
+        self.strategy = strategy
+        self.food_amount = 0
+        self.generation = 1
+        self.alive = True
+
+    @staticmethod
+    def increase_id_counter():
+        if Creature.id_counter > 999999:
+            raise ValueError("Creature limit reached")
+        Creature.id_counter += 1
