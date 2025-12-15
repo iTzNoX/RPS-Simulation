b230b247236681cb2244fc33f572b9ea0e07a682
Timothy Kropp <timothy.kropp@fcbup.de>
2025-12-15 19:55:58 +0100

    Adds Documentation to Creature

--- Project/Creature.py
+++ Project/Creature.py
@@ -1,7 +1,21 @@
 class Creature:
+    """
+    Represents a single creature in the simulation.
+    Handles everything regarding the Creatures
+    """
     id_counter = 0
 
     def __init__(self, strategy: str, generation: int = 1):
+        """
+        Initializes a Creature instance.
+
+        Attributes:
+            id (int): 6-digit long unique identifier of the creature
+            strategy (str): Has to be R, P or S, representing Rock, Paper or Scissors.
+            food_amount (int): the current amount of food the creatues owns. Needed for reproduction.
+            generation (int): current generation of the creature.
+            alive (bool): defines if the Creature is currently alive or not.
+        """
         Creature.increase_id_counter()
         self.id = f"{Creature.id_counter:06d}"
         self.food_amount = 0
@@ -19,7 +33,16 @@ class Creature:
 
     def kill(self):
         self.alive = False
+
     def play(self, opponent) -> str:
+        """
+        Plays one round against another creature.
+
+        Arguments:
+            opponent (Creature): The opposing creature.
+        Returns:
+            str: 'win', 'lose', or 'draw' depending on the result.
+        """
         rules = {"S": "P", "P": "R", "R": "S"}
         #draw
         if self.strategy == opponent.strategy:
@@ -34,6 +57,14 @@ class Creature:
             return "lose"
 
     def reproduce(self):
+        """
+        Generates offspring based on current food_amount.
+        Each food unit produces one child with the same strategy.
+        The parent creature dies after reproduction.
+
+        Returns:
+            list[Creature]: List of offspring creatures.
+        """
         offspring = []
         for _ in range(self.food_amount):
             child = Creature(strategy = self.strategy,
